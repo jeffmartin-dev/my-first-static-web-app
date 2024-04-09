c02672c72a5117d9d21af82bc54fd78f03a96081
Jeff Martin <jeff_martin@archibus.com>
2024-04-08 22:36:04 -0400

    add styled components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 400px;
+  height: 400px;
+  border: 2px solid red;
+`;
 
 function App() {
   const value = 'Jeff';
-  return <div>Hello {value}</div>;
+  return (
+    <Wrapper>
+      <div>Hello {value}</div>
+    </Wrapper>
+  );
 }
 
 export default App;
